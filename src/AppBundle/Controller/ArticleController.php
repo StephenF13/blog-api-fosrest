8b8d20efa40b2c392d4f979efe76387a98b73511
Stephen <stephenfauvergue@gmail.com>
2018-12-19 20:51:26 +0100

    use Hateoas Bundle and versioning Article

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -55,16 +55,16 @@ class ArticleController extends FOSRestController
             throw new ResourceValidationException($message);
         }
 
-            $em = $this->getDoctrine()->getManager();
+        $em = $this->getDoctrine()->getManager();
 
-            $em->persist($article);
-            $em->flush();
+        $em->persist($article);
+        $em->flush();
 
-            return $this->view($article, Response::HTTP_CREATED, [
-                'Location' => $this->generateUrl('app_article_show',
-                    ['id' => $article->getId(), UrlGeneratorInterface::ABSOLUTE_URL]),
-            ]);
-        }
+        return $this->view($article, Response::HTTP_CREATED, [
+            'Location' => $this->generateUrl('app_article_show',
+                ['id' => $article->getId(), UrlGeneratorInterface::ABSOLUTE_URL]),
+        ]);
+    }
 // Exemple utilisant le formulaire au lieu du body converter
 //    /**
 //     * @Rest\Post(
@@ -88,44 +88,94 @@ class ArticleController extends FOSRestController
 //        return $this->view($article, Response::HTTP_CREATED, ['Location' => $this->generateUrl('app_article_show', ['id' => $article->getId()])]);
 //    }
 
-        /**
-         * @Rest\Get("/articles", name="app_article_list")
-         * @Rest\QueryParam(
-         *     name="keyword",
-         *     requirements="[a-zA-Z0-9]",
-         *     nullable=true,
-         *     description="The keyword to search for."
-         * )
-         * @Rest\QueryParam(
-         *     name="order",
-         *     requirements="asc|desc",
-         *     default="asc",
-         *     description="Sort order (asc or desc)"
-         * )
-         * @Rest\QueryParam(
-         *     name="limit",
-         *     requirements="\d+",
-         *     default="15",
-         *     description="Max number of movies per page."
-         * )
-         * @Rest\QueryParam(
-         *     name="offset",
-         *     requirements="\d+",
-         *     default="0",
-         *     description="The pagination offset"
-         * )
-         * @Rest\View()
-         */
-        public
-        function listAction(ParamFetcherInterface $paramFetcher)
-        {
-            $pager = $this->getDoctrine()->getRepository('AppBundle:Article')->search(
-                $paramFetcher->get('keyword'),
-                $paramFetcher->get('order'),
-                $paramFetcher->get('limit'),
-                $paramFetcher->get('offset')
-            );
 
-            return new Articles($pager);
-        }
+    /**
+     * @Rest\Delete(
+     *     path="/articles/{id}",
+     *     name="app_article_delete",
+     *     requirements={"id"="\d+"}
+     *       )
+     * @Rest\View(StatusCode=204)
+     */
+    public function deleteAction(Article $article)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($article);
+        $em->flush();
     }
+
+    /**
+     * @Rest\Put(
+     *     path="/articles/{id}",
+     *     name="app_article_edit",
+     *     requirements={"id"="\d+"}
+     *     )
+     * @Rest\View(StatusCode = 200)
+     * @ParamConverter(
+     *     "newArticle",
+     *     converter="fos_rest.request_body",
+     *     options={
+     *         "validator"={ "groups"="Create" }
+     *     }
+     * )
+     */
+    public function editArticle(Article $article, Article $newArticle, ConstraintViolationList $violations)
+    {
+        if (count($violations)) {
+            $message = 'The JSON sent contains invalid data. Here are the errors you need to correct: ';
+            foreach ($violations as $violation) {
+                $message .= sprintf("Field %s: %s ", $violation->getPropertyPath(), $violation->getMessage());
+            }
+
+            throw new ResourceValidationException($message);
+        }
+
+        $article->setTitle($newArticle->getTitle());
+        $article->setContent($newArticle->getContent());
+
+        $em = $this->getDoctrine()->getManager();
+        $em->flush();
+
+        return $this->view($article, Response::HTTP_OK );
+    }
+
+    /**
+     * @Rest\Get("/articles", name="app_article_list")
+     * @Rest\QueryParam(
+     *     name="keyword",
+     *     requirements="[a-zA-Z0-9]",
+     *     nullable=true,
+     *     description="The keyword to search for."
+     * )
+     * @Rest\QueryParam(
+     *     name="order",
+     *     requirements="asc|desc",
+     *     default="asc",
+     *     description="Sort order (asc or desc)"
+     * )
+     * @Rest\QueryParam(
+     *     name="limit",
+     *     requirements="\d+",
+     *     default="15",
+     *     description="Max number of movies per page."
+     * )
+     * @Rest\QueryParam(
+     *     name="offset",
+     *     requirements="\d+",
+     *     default="0",
+     *     description="The pagination offset"
+     * )
+     * @Rest\View()
+     */
+    public function listAction(ParamFetcherInterface $paramFetcher)
+    {
+        $pager = $this->getDoctrine()->getRepository('AppBundle:Article')->search(
+            $paramFetcher->get('keyword'),
+            $paramFetcher->get('order'),
+            $paramFetcher->get('limit'),
+            $paramFetcher->get('offset')
+        );
+
+        return new Articles($pager);
+    }
+}
--- src/AppBundle/Entity/Article.php
+++ src/AppBundle/Entity/Article.php
@@ -7,6 +7,7 @@ use JMS\Serializer\Annotation as Serializer;
 use JMS\Serializer\Annotation\ExclusionPolicy;
 use JMS\Serializer\Annotation\Expose;
 use Symfony\Component\Validator\Constraints as Assert;
+use Hateoas\Configuration\Annotation as Hateoas;
 
 
 /**
@@ -15,6 +16,37 @@ use Symfony\Component\Validator\Constraints as Assert;
  *
  * @ExclusionPolicy("all")
  *
+ *
+ * @Hateoas\Relation(
+ *      "self",
+ *      href = @Hateoas\Route(
+ *          "app_article_show",
+ *          parameters = { "id" = "expr(object.getId())" },
+ *          absolute = true
+ *      )
+ * )
+ *
+ * @Hateoas\Relation(
+ *      "edit",
+ *      href = @Hateoas\Route(
+ *          "app_article_edit",
+ *          parameters = { "id" = "expr(object.getId())" },
+ *          absolute = true
+ *      )
+ * )
+ * @Hateoas\Relation(
+ *      "delete",
+ *      href = @Hateoas\Route(
+ *          "app_article_delete",
+ *          parameters = { "id" = "expr(object.getId())" },
+ *          absolute = true
+ *      )
+ * )
+ *
+ * @Hateoas\Relation(
+ *     "author",
+ *     embedded = @Hateoas\Embedded("expr(object.getAuthor())")
+ * )
  */
 class Article
 {
@@ -30,6 +62,7 @@ class Article
     /**
      * @ORM\Column(type="string", length=100)
      * @Expose
+     * @Serializer\Since("1.0")
      *
      * @Assert\NotBlank(groups={"Create"})
      */
@@ -38,10 +71,18 @@ class Article
     /**
      * @ORM\Column(type="text")
      * @Expose
+     * @Serializer\Since("1.0")
      *
      * @Assert\NotBlank(groups={"Create"})
      */
     private $content;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     * @Serializer\Expose
+     * @Serializer\Since("2.0")
+     */
+    private $shortDescription;
 
     /**
      * @ORM\ManyToOne(targetEntity="Author", cascade={"all"}, fetch="EAGER")
@@ -85,5 +126,21 @@ class Article
     public function setAuthor(Author $author)
     {
         $this->author = $author;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getShortDescription()
+    {
+        return $this->shortDescription;
+    }
+
+    /**
+     * @param mixed $shortDescription
+     */
+    public function setShortDescription($shortDescription)
+    {
+        $this->shortDescription = $shortDescription;
     }
 }
